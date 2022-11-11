f61d794b5f069b58c5f1fa337a1a1463b9f34221
Joao Vitor <joaovitorti07@gmail.com>
2022-11-10 21:53:27 -0300

    Aula 15. Union Types

---
+++ output/tipos_de_dados.js
@@ -0,0 +1,10 @@
+"use strict";
+var ano = "2010";
+var ano2 = Number(ano);
+console.log(ano, ano2);
+console.log("================================================");
+var chavePix;
+chavePix = "email@gmail.com";
+chavePix = 88981545847;
+console.log(chavePix);
+//# sourceMappingURL=tipos_de_dados.js.map
---
+++ scripts/tipos_de_dados.ts
@@ -0,0 +1,14 @@
+let ano: string = "2010";
+
+let ano2: number = Number(ano);
+
+console.log(ano, ano2);
+
+console.log("================================================");
+
+let chavePix: string | number;
+
+chavePix = "email@gmail.com";
+chavePix = 88981545847;
+
+console.log(chavePix);
