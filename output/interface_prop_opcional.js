a2e62045125b7100b4136847c44dd8cc64f78666
Joao Vitor <joaovitorti07@gmail.com>
2022-11-16 21:48:26 -0300

    Aula 31. Propriedades opcionais das interfaces

---
+++ output/interface_prop_opcional.js
@@ -0,0 +1,8 @@
+"use strict";
+const produtoDados = {
+    nome: "Notebook",
+    preco: 2000,
+    dataValidade: new Date(2022, 11, 1)
+};
+console.log(produtoDados);
+//# sourceMappingURL=interface_prop_opcional.js.map
