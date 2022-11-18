87864a7cecf588bde0b03ca66a91b940814b4fe0
Joao Vitor <joaovitorti07@gmail.com>
2022-11-17 23:59:54 -0300

    Aula 37. Criando a primeira classe

---
+++ scripts/primeira_classe.ts
@@ -0,0 +1,34 @@
+class Pessoa {
+
+    nome: string;
+    idade: number;
+    altura: number;
+
+    constructor(nome: string, idade: number, altura: number) {
+
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+
+    }
+
+    public setNome(nome: string): void {
+        this.nome = nome;
+    }
+
+    toString(): string {
+
+        return `A pessoa ${this.nome} tem ${this.idade} anos e mede ${this.altura} de altura.`;
+
+    }
+
+}
+
+
+const ronaldo = new Pessoa("Ronaldo Braz", 30, 1.85);
+const marcus = new Pessoa("Marcus Ribeiro", 19, 2);
+
+ronaldo.nome = "Ronaldo";
+
+console.log("Pessoa: " + ronaldo);
+console.log("Pessoa: " + marcus);
