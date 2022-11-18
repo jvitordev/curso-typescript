0bb07c1d2e7ba185044f56de388109c486a7902a
Joao Vitor <joaovitorti07@gmail.com>
2022-11-17 22:37:41 -0300

    Aula 35. Estendendo Interfaces

---
+++ output/interface_extend.js
@@ -0,0 +1,10 @@
+"use strict";
+const joao = {
+    email: "joao@hcode.com.br",
+    id: 1,
+    nome: "João Rangel",
+    senha: "123456",
+    created_at: new Date().getTime(),
+    updated_at: new Date().getTime()
+};
+//# sourceMappingURL=interface_extend.js.map
---
+++ output/primeira_classe.js
@@ -0,0 +1,20 @@
+"use strict";
+class Pessoa {
+    nome;
+    idade;
+    altura;
+    constructor(nome, idade, altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+    toString() {
+        return `A pessoa ${this.nome} tem ${this.idade} anos e mede ${this.altura} de altura.`;
+    }
+}
+const ronaldo = new Pessoa("Ronaldo Braz", 30, 1.85);
+const marcus = new Pessoa("Marcus Ribeiro", 19, 2);
+ronaldo.nome = "Ronaldo";
+console.log("Pessoa: " + ronaldo);
+console.log("Pessoa: " + marcus);
+//# sourceMappingURL=primeira_classe.js.map
