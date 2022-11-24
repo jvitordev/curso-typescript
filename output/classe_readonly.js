7cb5ad596ee1185d761c17950679374db6ac1190
Joao Vitor <joaovitorti07@gmail.com>
2022-11-23 22:07:13 -0300

    Aula 44. private x readonly

---
+++ output/classe_readonly.js
@@ -0,0 +1,9 @@
+"use strict";
+class Usuario {
+    id = "123123sdad232";
+    nome = "Joao Vitor";
+    senha = "123456";
+    dataCadastro = new Date("2021-01-01");
+}
+const usuario = new Usuario();
+//# sourceMappingURL=classe_readonly.js.map
