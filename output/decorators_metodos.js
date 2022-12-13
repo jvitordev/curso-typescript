ccc67dc9475cb8d3119b6a393978624a1037b959
Joao Vitor <joaovitorti07@gmail.com>
2022-12-13 12:30:09 -0300

    Aula 64. Decorators em métodos

---
+++ output/decorators_metodos.js
@@ -0,0 +1,23 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function decoratorMetodo(target, propertyKey, descriptor) {
+    descriptor.value = (...args) => {
+        return args.map(item => item.toLowerCase());
+    };
+}
+class TratarMensagem {
+    dizerMensagem(...mensagens) {
+        return mensagens;
+    }
+}
+__decorate([
+    decoratorMetodo
+], TratarMensagem.prototype, "dizerMensagem", null);
+const instancia = new TratarMensagem();
+console.log(instancia.dizerMensagem("Olá", "Seja bem-vindo", "Hcode"));
+//# sourceMappingURL=decorators_metodos.js.map
